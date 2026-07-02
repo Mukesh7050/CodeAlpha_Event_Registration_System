632b188cb72857dc92ad7abda688ad1e7a54c92e
Mukesh7050 <mukeshkumar11457.rr@gmail.com>
2026-07-02 18:30:21 +0530

    Fixed new  events API URL

--- Frontend/js/events.js
+++ Frontend/js/events.js
@@ -1,4 +1,4 @@
-const API_URL_URL = "https://codealpha-event-registration-system-qxlu.onrender.com";
+const API_URL = "https://codealpha-event-registration-system-qxlu.onrender.com";
 // ==========================
 // Load Events
 // ==========================
